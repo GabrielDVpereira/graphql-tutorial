b4c2982f306e7edd4ebf298ff690d8bc95add237
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-03 18:09:25 -0300

    Params and arguments!

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ const {buildSchema} = require('graphql');
 const schema = buildSchema(
   `
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
   `
-)
+);
 
 const root = {
-  hello: () => {
-    return 'Hello world!'
-  }
-}
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+  }, 
+  random: () => {
+    return Math.random();
+  },
+  rollDice: ({numDice, numSides}) => {
+    var output = []; 
+    for(let i = 0; i< numDice; i++){
+      output.push(1+Math.floor(Math.random() * (numSides || 6)))
+    }
+
+    return output
+  } 
+};
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
